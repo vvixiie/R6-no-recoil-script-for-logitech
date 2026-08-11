38be55aba3e61e385959179939fb82f2844b43ca
aynoa310-bit <aynoa310@gmail.com>
2026-08-11 12:30:07 -0400

    Update NO RECOIL.lua

--- NO RECOIL.lua
+++ NO RECOIL.lua
@@ -3,7 +3,7 @@ EnableRC          = true                  -- Master switch
 RequireToggle     = true                  -- Require toggle key
 ToggleKey         = "CapsLock"            -- Toggle key (e.g., "CapsLock")
 
-RecoilControlMode = "ECHO"                -- Select operator preset
+RecoilControlMode = "ASH"                -- Select operator preset
 
 -- =============== RECOIL VALUES (Presets) ===============
 local RecoilPresets = {
